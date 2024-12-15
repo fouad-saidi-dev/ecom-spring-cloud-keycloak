e0185940036162a22736d565d74888d93b3214cb
FOUAD SAIDI <fouadsai17@gmail.com>
2024-12-15 17:13:02 +0100

    delete inventory-service

--- inventory-service/src/main/java/com/fouadev/inventoryservice/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fouadev.inventoryservice.repositories;
-
-import com.fouadev.inventoryservice.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
